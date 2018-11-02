deb0559c869d0acbc50a42a2fdc00f2f76d5e4d3
Vladan <vladanvelojic@gmail.com>
2018-11-02 21:15:09 +0100

    all main functionalities added, just finish the game with details like: disabled buttons and maybe timer

--- index.js
+++ index.js
@@ -50,14 +50,14 @@ let combinationsArr = [
 ]
 
 const boxesNode = document.querySelectorAll('.box')
-const boxesArr = [...boxesNode]
+// const boxesArr = [...boxesNode]
 
 // shuffled array
-const shuffledArr = shuffle([...combinationsArr])
+const shuffledArr = shuffle(combinationsArr)
 
 // populate grid with random numbers
-for(let i = 0; i < boxesArr.length; i++) {
-    let box = boxesArr[i]
+for(let i = 0; i < boxesNode.length; i++) {
+    let box = boxesNode[i]
     for(let j = 0; j < shuffledArr.length; j++) {
         let combination = shuffledArr[i].num
         box.innerHTML = combination
@@ -65,13 +65,16 @@ for(let i = 0; i < boxesArr.length; i++) {
 }
 
 // first click
-for(let i = 0; i < boxesArr.length; i++) {
-    boxesArr[i].addEventListener('click', click)
+for(let i = 0; i < boxesNode.length; i++) {
+    boxesNode[i].addEventListener('click', click)
 }
 
 let clicks = null
 let firstClick
 let secondClick
+
+let firstObjectIndex
+let secondObjectIndex
 
 function click(e) {
     // debugger
@@ -80,26 +83,73 @@ function click(e) {
     if(clicks === 1) {
         firstClick = e.target.innerHTML
         e.target.classList.add('clicked')
-        console.log(firstClick)
+        firstClickCLass = e.target
+
+        // find index of ELEMENT
+        let firstElIndex;
+        console.log(shuffledArr)
+        for(let i = 0; i < boxesNode.length; i++){
+            if(boxesNode[i] === firstClickCLass) {
+                firstElIndex = i;
+                console.log('prvi index',firstElIndex)
+            }
+        }
+
+        //find OBJECT by element index
+        firstObjectIndex = shuffledArr[firstElIndex]
+        firstObjectIndex.clicked = true;
+        console.log(firstObjectIndex)
+        
     } 
+
     if(clicks === 2) {
         secondClick = e.target.innerHTML
         e.target.classList.add('clicked')
-        e.target.classList.add('win')
-        console.log(secondClick)
+        secondClickCLass = e.target
+
+        // find index of ELEMENT
+        let secondElIndex;
+        console.log(shuffledArr)
+        for(let i = 0; i < boxesNode.length; i++){
+            if(boxesNode[i] === secondClickCLass) {
+                secondElIndex = i;
+                console.log('drugi index', secondElIndex)
+            }
+        }
+
+        //find OBJECT by element index
+        secondObjectIndex = shuffledArr[secondElIndex]
+        secondObjectIndex.clicked = true;
+        console.log(secondObjectIndex)
+
+        //check if they match
         if(secondClick === firstClick){
-            console.log('pogodili smo')
+            firstObjectIndex.solved = true
+            secondObjectIndex.solved = true
             clicks = 0
+            console.log('Pogodili smo')
         } else {
+            firstObjectIndex.clicked = true
+            secondObjectIndex.clicked = true
+            console.log('Promasili smo')
             clicks = 0
-            for(let i = 0; i < boxesArr.length; i++) {
-                if(boxesArr[i].classList.contains('clicked')){
+
+            //close boxes because they dont match
+            //return clicked property to false
+            for(let i = 0; i < shuffledArr.length; i++) {
+                if(shuffledArr[i].solved) {
+                    boxesNode[i].classList.add('win')
+                }
+                if(shuffledArr[i].clicked && boxesNode[i].classList.contains('clicked')) {
                     const timer = setTimeout(() => {
-                        boxesArr[i].classList.remove('clicked')
+                        boxesNode[i].classList.remove('clicked')
+                        shuffledArr[i].clicked = false
+                        console.log(shuffledArr[i])
                     }, 2000)
+                    
+
                 }
             }
-            console.log(clicks)
         }
     }
 
